04f61da5e5c674c50ec08ad0b08f513d9e5bfa34
Vicky Thakur <52929183+vickythakur0398@users.noreply.github.com>
2020-07-23 19:20:16 +0530

    sell_and_buy_stocks_all _companies_must

--- sell_sstock_all_companies_4E.java
+++ sell_sstock_all_companies_4E.java
@@ -1,3 +1,36 @@
+/*
+The cost of stock on each day is given in an array A[] of size N. Find all the days on which you buy and sell the stock so that in between those days your profit is maximum.
+
+Input: 
+First line contains number of test cases T. First line of each test case contains an integer value N denoting the number of days, followed by an array of stock prices of N days. 
+
+Output:
+For each testcase, output all the days with profit in a single line. And if there is no profit then print "No Profit".
+
+Constraints:
+1 <= T <= 100
+2 <= N <= 103
+0 <= Ai <= 104
+
+Example
+Input:
+3
+7
+100 180 260 310 40 535 695
+4
+100 50 30 20
+10
+23 13 25 29 33 19 34 45 65 67
+
+Output:
+(0 3) (4 6)
+No Profit
+(1 4) (5 9)
+
+
+*/
+
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
